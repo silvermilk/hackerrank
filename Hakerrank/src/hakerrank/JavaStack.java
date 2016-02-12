98a3b0727d105d48d04bc199e334207f41ac297a
Anastasia <anchu@n103513.merann.ru>
2016-02-12 12:51:18 +0300

    JavaStack

--- Hakerrank/src/hakerrank/JavaStack.java
+++ Hakerrank/src/hakerrank/JavaStack.java
@@ -2,10 +2,10 @@ package hakerrank;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class JavaStack {
 
@@ -14,14 +14,15 @@ public class JavaStack {
     public static void main(String[] argh) {
         Scanner sc = new Scanner(System.in);
 
-        relations.put(')', '(');
-        relations.put('}', '{');
-        relations.put(']', '[');
+        relations.put('(', ')');
+        relations.put('{', '}');
+        relations.put('[', ']');
 
-        LinkedList<Character> stack = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
         List<Boolean> balancedList = new ArrayList<>();
         int n = sc.nextInt();
         sc.nextLine();
+        Boolean isBalanced;
         for (int i = 0; i < n; i++) {
 
             String input = sc.next();
@@ -29,46 +30,25 @@ public class JavaStack {
 
             stack.clear();
             for (Character chr : input.toCharArray()) {
-                stack.addLast(chr);
+                if(relations.containsKey(chr)) {
+                    stack.push(chr);
+                } else if(!stack.isEmpty()){
+                    Character startChr = stack.peek();
+                    if(relations.get(startChr).equals(chr)) {
+                        stack.pop();
+                    }
+                } else {
+                    stack.push(chr);
+                    break;
+                }
             }
 
-            balancedList.add(isBalanced(stack));
+            isBalanced = stack.isEmpty();
+            balancedList.add(isBalanced);
         }
         for (boolean balanced : balancedList) {
             System.out.println(balanced);
         }
 
     }
-
-    private static boolean isBalanced(LinkedList<Character> stack) {
-
-        while (!stack.isEmpty()) {
-            Character endChar = stack.getLast();
-            Character startChar = relations.get(endChar);
-
-            int startIndex = stack.lastIndexOf(startChar);
-            if (startIndex == -1) {
-                return false;
-            } else {
-
-                LinkedList<Character> sublist = new LinkedList(stack.subList(startIndex, stack.size()-1));
-                if (sublist.contains(endChar)) {
-                    int endIndex = sublist.indexOf(endChar);
-                    if (endIndex == 1) {
-                        stack.remove(startIndex+1);
-                        stack.remove(startIndex);
-                    } else {
-                        return isBalanced(new LinkedList(stack.subList(startIndex + 1, startIndex + endIndex)));
-                    }
-
-                } else {
-                    stack.remove(endChar);
-                    stack.remove(startChar);
-                }
-
-            }
-        }
-        return true;
-    }
-
 }
